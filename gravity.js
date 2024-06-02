cf08425c86e545fbc5a7f302124c1358b1690cc0
Saverio Dieni <saveriodieni@gmail.com>
2024-06-02 17:22:27 +0200

    better collisions

--- gravity.js
+++ gravity.js
@@ -34,19 +34,23 @@ class Vec3 {
 function ToVec3(a) { return new Vec3(a[0],a[1],a[2]); }
 
 var iteration;
+var holesQueue;
 
 // This function is called for every step of the simulation.
 // Its job is to advance the simulation for the given time step duration dt.
 // It updates the given positions and velocities.
-function SimTimeStep(dt, positions, radii, velocities, muS, muD, particleMass, gravity, restitution, holes) {
+function SimTimeStep(dt, positions, radii, velocities, muS, muD, particleMass, gravity, restitution, holes, omega , momentOfInertia) {
     function nearToHole(index){
         for(var i=0;i<holes.length;i++){
-            if((positions[index].sub(holes[i])).len()<0.4) return true;
+            if((positions[index].sub(holes[i])).len()<0.5) return true;
         }
         return false;
     }
     var forces = Array(positions.length); // The total force per particle
-    if (iteration === undefined) iteration = 0;
+    if (iteration === undefined){
+        iteration = 0;
+        holesQueue = Array(6).fill().map(() => []);
+    }
     else iteration++;
     
     // Compute the total force for each particle
@@ -78,48 +82,28 @@ function SimTimeStep(dt, positions, radii, velocities, muS, muD, particleMass, g
             var dynamicFriction = frictionDirection.mul(dynamicFrictionMagnitude);
             forces[i] = forces[i].add(dynamicFriction);
         }
+
+        if (velocities[i].len() > 0.01) {
+			let frictionForce = gravity.len() * muD * particleMass;
+			let frictionTorque = velocities[i].mul(frictionForce * radii[i]);
+			let angularAcceleration = frictionTorque.div(momentOfInertia);
+			omega[i] = omega[i].add(angularAcceleration.mul(dt));
+		}
     }
 
-    // Update positions and velocities
-    for (var i = 0; i < positions.length; i++) {
-        var a = forces[i].div(particleMass);
-        velocities[i] = velocities[i].add(a.mul(dt));
-        positions[i] = positions[i].add(velocities[i].mul(dt));
-    }
+    // Update positions, velocities, and angular velocities
+	for (var i = 0; i < positions.length; i++) {
+		var a = forces[i].div(particleMass);
+		velocities[i] = velocities[i].add(a.mul(dt));
+		positions[i] = positions[i].add(velocities[i].mul(dt));
 
-    // Handle collisions with ground and walls
-    for (var i = 0; i < positions.length; i++) {
-        if((Math.abs(positions[i].x)+radii[i])>2.7 && (Math.abs(positions[i].y)+radii[i])>5.25){ // {up-left,up-right,bottom-left,bottom-right} hole
-            if(nearToHole(i)){
-                positions[i].x=3.0*Math.sign(positions[i].x);
-                positions[i].y=5.55*Math.sign(positions[i].y);
-                if(positions[i].z>-1.0){velocities[i]=new Vec3(0,0,gravity.z*particleMass);}
-                else velocities[i]=new Vec3(0,0,0);
-            }
-        }
+		// Apply rolling resistance to slow down spin
+		let rollingResistance = muD * gravity.len() * radii[i] / momentOfInertia;
+		omega[i] = omega[i].mul(1 - rollingResistance * dt);
 
-        else if((Math.abs(positions[i].y)+radii[i])<0.3 && (Math.abs(positions[i].x)+radii[i])>2.85){ // {left,right} hole
-            if(nearToHole(i)){
-                positions[i].x=3.0*Math.sign(positions[i].x);
-                positions[i].y=0.0;
-                if(positions[i].z>-1.0){velocities[i]=new Vec3(0,0,gravity.z*particleMass);}
-                else {
-                    positions[i].z=-1.0;
-                    velocities[i]=new Vec3(0,0,0);
-                }
-            }
-        }
-
-        else if((Math.abs(positions[i].x)+radii[i])>=2.85){//{left,right} wall
-            positions[i].x=2.85*Math.sign(positions[i].x)-radii[i]*Math.sign(positions[i].x);
-            velocities[i].x = -velocities[i].x * restitution;
-        }
-        
-        else if((Math.abs(positions[i].y)+radii[i])>=5.4){//{bottom,up} wall
-            positions[i].y=5.4*Math.sign(positions[i].y)-radii[i]*Math.sign(positions[i].y);
-            velocities[i].y = -velocities[i].y * restitution;
-        }
-    }
+		// Apply linear motion due to spin (gyroscopic effect)
+		velocities[i] = velocities[i].add(omega[i].mul(dt*radii[i]));
+	}
 
     // Handle collisions between spheres
     for (let i = 0; i < positions.length; i++) {
@@ -147,14 +131,189 @@ function SimTimeStep(dt, positions, radii, velocities, muS, muD, particleMass, g
         }
     }
 
-    // Apply small velocity threshold to stop the particle
+    // Handle collisions with ground and walls
     for (var i = 0; i < positions.length; i++) {
-        if (Math.abs(velocities[i].x) < 0.01) velocities[i].x = 0;
-        if (Math.abs(velocities[i].y) < 0.01) velocities[i].y = 0;
-        if (Math.abs(velocities[i].z) < 0.01) velocities[i].z = 0;
-        if(positions[i].z<-1.0){
-            positions[i].z=-1.0;
-            velocities[i]=new Vec3(0,0,0);
+        if((Math.abs(positions[i].x)+radii[i])>2.7 && (Math.abs(positions[i].y)+radii[i])>5.25){ // {up-left,up-right,bottom-left,bottom-right} hole
+            if(nearToHole(i)){
+                positions[i].x=3.0*Math.sign(positions[i].x);
+                if(positions[i].z>=0.0) positions[i].z=-0.2;
+                positions[i].y=5.55*Math.sign(positions[i].y);
+                if(positions[i].z>-1.0){velocities[i]=new Vec3(0,0,gravity.z*particleMass);}
+                else velocities[i]=new Vec3(0,0,0);
+            }
+            else{
+                if((Math.abs(positions[i].x)+radii[i])>=2.85){//{left,right} wall
+                    var lengthPos=velocities[i].len();
+                    var holeCoordinates=new Vec3(3.0*Math.sign(positions[i].x),5.55*Math.sign(positions[i].y),0);
+                    var lengthHole=holeCoordinates.len();
+                    cosTheta=(positions[i].dot(holeCoordinates))/(lengthHole*lengthPos);
+                    if(Math.abs(cosTheta)<=1.0){
+                        sinTheta=Math.sqrt(1-cosTheta*cosTheta);
+                        velocities[i].x = lengthPos*cosTheta*restitution*Math.sign(velocities[i].x);
+                        velocities[i].y = lengthPos*sinTheta*restitution*Math.sign(velocities[i].y);
+                    }
+                }
+                
+                else if((Math.abs(positions[i].y)+radii[i])>=5.4){//{bottom,up} wall
+                    var lengthPos=velocities[i].len();
+                    var holeCoordinates=new Vec3(3.0*Math.sign(positions[i].x),5.55*Math.sign(positions[i].y),0);
+                    var lengthHole=holeCoordinates.len();
+                    cosTheta=(positions[i].dot(holeCoordinates))/(lengthHole*lengthPos);
+                    if(Math.abs(cosTheta)<=1.0){
+                        sinTheta=Math.sqrt(1-cosTheta*cosTheta);
+                        velocities[i].x = lengthPos*cosTheta*restitution*Math.sign(velocities[i].x);
+                        velocities[i].y = lengthPos*sinTheta*restitution*Math.sign(velocities[i].y);
+                    }
+                }
+            }
+        }
+
+        else if((Math.abs(positions[i].y)+radii[i])<0.3 && (Math.abs(positions[i].x)+radii[i])>2.85){ // {left,right} hole
+            if(nearToHole(i)){
+                positions[i].x=3.0*Math.sign(positions[i].x);
+                if(positions[i].z>=0.0) positions[i].z=-0.2;
+                positions[i].y=0.0;
+                if(positions[i].z>-1.0){velocities[i]=new Vec3(0,0,gravity.z*particleMass);}
+                else {
+                    positions[i].z=-1.0;
+                    velocities[i]=new Vec3(0,0,0);
+                }
+            }
+            else{
+                if((Math.abs(positions[i].x)+radii[i])>=2.85){//{left,right} wall
+                    var lengthPos=velocities[i].len();
+                    var holeCoordinates=new Vec3(3.0*Math.sign(positions[i].x),5.55*Math.sign(positions[i].y),0);
+                    var lengthHole=holeCoordinates.len();
+                    cosTheta=(positions[i].dot(holeCoordinates))/(lengthHole*lengthPos);
+                    if(Math.abs(cosTheta)<=1.0){
+                        sinTheta=Math.sqrt(1-cosTheta*cosTheta);
+                        velocities[i].x = lengthPos*cosTheta*restitution*Math.sign(velocities[i].x);
+                        velocities[i].y = lengthPos*sinTheta*restitution*Math.sign(velocities[i].y);
+                    }
+                }
+                
+                else if((Math.abs(positions[i].y)+radii[i])>=5.4){//{bottom,up} wall
+                    var lengthPos=velocities[i].len();
+                    var holeCoordinates=new Vec3(3.0*Math.sign(positions[i].x),5.55*Math.sign(positions[i].y),0);
+                    var lengthHole=holeCoordinates.len();
+                    cosTheta=(positions[i].dot(holeCoordinates))/(lengthHole*lengthPos);
+                    if(Math.abs(cosTheta)<=1.0){
+                        sinTheta=Math.sqrt(1-cosTheta*cosTheta);
+                        velocities[i].x = lengthPos*cosTheta*restitution*Math.sign(velocities[i].x);
+                        velocities[i].y = lengthPos*sinTheta*restitution*Math.sign(velocities[i].y);
+                    }
+                }
+            }
+        }
+
+        else if((Math.abs(positions[i].x)+radii[i])>=2.85){//{left,right} wall
+            positions[i].x=2.85*Math.sign(positions[i].x)-radii[i]*Math.sign(positions[i].x);
+            velocities[i].x = -velocities[i].x * restitution;
+        }
+        
+        else if((Math.abs(positions[i].y)+radii[i])>=5.4){//{bottom,up} wall
+            positions[i].y=5.4*Math.sign(positions[i].y)-radii[i]*Math.sign(positions[i].y);
+            velocities[i].y = -velocities[i].y * restitution;
         }
     }
+
+    // Apply small velocity threshold to stop the particle
+    for (var i = 0; i < positions.length; i++) {
+		if (Math.abs(velocities[i].x) < 0.01) velocities[i].x = 0;
+		if (Math.abs(velocities[i].y) < 0.01) velocities[i].y = 0;
+		if (Math.abs(velocities[i].z) < 0.01) velocities[i].z = 0;
+		if (Math.abs(omega[i].x) < 0.01) omega[i].x = 0;
+		if (Math.abs(omega[i].y) < 0.01) omega[i].y = 0;
+		if (Math.abs(omega[i].z) < 0.01) omega[i].z = 0;
+		if(positions[i].z < -1.0) {
+			positions[i].z = -1.0;
+            velocities[i].z = 0.0;
+		}
+        if(positions[i].z<0.0){
+            velocities[i].x = 0.0;
+            velocities[i].y= 0.0;
+        }
+	}
+
+    // Reset particles that fall into holes and add to the queue
+	for (var i = 0; i < positions.length; i++) {
+		if(positions[i].x==-3.0 && positions[i].y==5.55){
+            if(!holesQueue[0].includes(i)){
+                if(holesQueue[0].length<3){
+                    holesQueue[0].push(i);
+                }
+                else{
+                    var ballIndex=holesQueue[0].shift();
+                    holesQueue[0].push(i);
+                    positions[ballIndex]=new Vec3(Math.random(),Math.random(),-1);
+                    velocities[ballIndex]=new Vec3(0,0,0);
+                }
+            }
+        }
+        else if(positions[i].x==3.0 && positions[i].y==5.55){
+            if(!holesQueue[1].includes(i)){
+                if(holesQueue[1].length<3){
+                    holesQueue[1].push(i);
+                }
+                else{
+                    var ballIndex=holesQueue[1].shift();
+                    holesQueue[1].push(i);
+                    positions[ballIndex]=new Vec3(Math.random(),Math.random(),-1);
+                    velocities[ballIndex]=new Vec3(0,0,0);
+                }
+            }
+        }
+        else if(positions[i].x==-3.0 && positions[i].y==0.0){
+            if(!holesQueue[2].includes(i)){
+                if(holesQueue[2].length<3){
+                    holesQueue[2].push(i);
+                }
+                else{
+                    var ballIndex=holesQueue[2].shift();
+                    holesQueue[2].push(i);
+                    positions[ballIndex]=new Vec3(Math.random(),Math.random(),-1);
+                    velocities[ballIndex]=new Vec3(0,0,0);
+                }
+            }
+        }
+        else if(positions[i].x==3.0 && positions[i].y==0.0){
+            if(!holesQueue[3].includes(i)){
+                if(holesQueue[3].length<3){
+                    holesQueue[3].push(i);
+                }
+                else{
+                    var ballIndex=holesQueue[3].shift();
+                    holesQueue[3].push(i);
+                    positions[ballIndex]=new Vec3(Math.random(),Math.random(),-1);
+                    velocities[ballIndex]=new Vec3(0,0,0);
+                }
+            }
+        }
+        else if(positions[i].x==-3.0 && positions[i].y==-5.55){
+            if(!holesQueue[4].includes(i)){
+                if(holesQueue[4].length<3){
+                    holesQueue[4].push(i);
+                }
+                else{
+                    var ballIndex=holesQueue[4].shift();
+                    holesQueue[4].push(i);
+                    positions[ballIndex]=new Vec3(Math.random(),Math.random(),-1);
+                    velocities[ballIndex]=new Vec3(0,0,0);
+                }
+            }
+        }
+        else if(positions[i].x==3.0 && positions[i].y==-5.55){
+            if(!holesQueue[5].includes(i)){
+                if(holesQueue[5].length<3){
+                    holesQueue[5].push(i);
+                }
+                else{
+                    var ballIndex=holesQueue[5].shift();
+                    holesQueue[5].push(i);
+                    positions[ballIndex]=new Vec3(Math.random(),Math.random(),-1);
+                    velocities[ballIndex]=new Vec3(0,0,0);
+                }
+            }
+        }		
+	}
 }
